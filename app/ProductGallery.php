442288b38225952a0a494cad9d0886d2fe92296e
butga <arifyuniartofajarbugaresa@gmail.com>
2021-01-11 19:00:35 +0700

    add CRUD Admin Product

---
+++ app/ProductGallery.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProductGallery extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'product_id', 
+        'photo', 
+        'is_default'
+    ];
+
+    protected $hidden = [
+
+    ];
+
+    public function product() 
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
+
+    public function getPhotoAtrribute($value) 
+    {
+        return url('storage/' . $value);
+    }
+
+}
